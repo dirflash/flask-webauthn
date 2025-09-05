91ce7bc2b7664c042d8509587fbbe0e90858c144
Aaron Davis <aarodavi@cisco.com>
2025-09-05 15:45:27 -0600

    Refactor User and WebAuthnCredential models for improved clarity and structure

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
 
 db = SQLAlchemy()
 
@@ -18,6 +19,22 @@ class User(db.Model):  # type: ignore
     username = db.Column(db.String(255), unique=True, nullable=False)
     name = db.Column(db.String(255), nullable=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
+    credentials = db.relationship(
+        "WebAuthnCredential", backref=backref("user", cascade="all, delete"), lazy=True
+    )
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+
+class WebAuthnCredential(db.Model):
+    """Stored WebAuthn Credentials as a replacement for passwords."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    credential_id = db.Column(db.LargeBinary, nullable=False)
+    credential_public_key = db.Column(db.LargeBinary, nullable=False)
+    current_sign_count = db.Column(db.Integer, default=0)
+
+    def __repr__(self):
+        return f"<Credential {self.credential_id}>"
