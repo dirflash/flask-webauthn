7a5260b9d58f4c7709048b681799d16e1cec5559
Aaron Davis <aarodavi@cisco.com>
2025-09-05 15:20:14 -0600

    Changes to be committed: 	modified:   app/models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,22 @@
+import uuid
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+def _str_uuid():
+    return str(uuid.uuid4())
+
+
+class User(db.Model):
+    """A user in the database"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.String(40), default=_str_uuid, unique=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    name = db.Column(db.String(255), nullable=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
