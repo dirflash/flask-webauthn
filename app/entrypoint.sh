bbb2475f384e11fc0298adec976b0526a7215f5f
Aaron Davis <aarodavi@cisco.com>
2025-09-05 17:04:22 -0600

    Remove duplicate commands from entrypoint.sh

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,6 +2,3 @@
 wait-for-it -t 10 db:5432 && \
 flask db upgrade && \
 waitress-serve --host 0.0.0.0 --port 5000 app:app
-wait-for-it -t 10 db:5432 && \
-flask db upgrade && \
-waitress-serve --host 0.0.0.0 --port 5000 app:app
