f4eb99aac7eba074dfa4fb7efba5b91258ec75f0
Aaron Davis <aaron.e.davis@gmail.com>
2025-09-05 17:11:31 -0600

    Consolidate wait-for-it command in entrypoint.sh for improved readability

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-wait-for-it db:5432 -t 10
-flask db upgrade
-waitress-serve --host 0.0.0.0 --port 5000 app:app
 wait-for-it -t 10 db:5432 && \
 flask db upgrade && \
 waitress-serve --host 0.0.0.0 --port 5000 app:app
