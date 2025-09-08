f62410574aa9c4095231e5340a31bb7ce0012f79
Aaron Davis <aarodavi@cisco.com>
2025-09-08 15:05:00 -0600

    Add registration form and update register route to render template

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/register")
 def register():
-    return "Register user"
+    """Show the form to register a new user"""
+    return render_template("auth/register.html")
 
 
 @auth.route("/login")
